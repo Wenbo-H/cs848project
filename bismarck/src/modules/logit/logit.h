b51051103a410df093d61ec8d39ff856906b0ca8
Wenbo-H <43230254+Wenbo-H@users.noreply.github.com>
2019-07-26 12:00:13 -0400

    change back to beta, 1-beta

--- bismarck/src/modules/logit/logit.h
+++ bismarck/src/modules/logit/logit.h
@@ -17,6 +17,48 @@ limitations under the License.
 #ifndef LOGIT_H
 #define LOGIT_H
 
+
+inline void
+sparse_logit_grad(struct LinearModel *ptrModel, const int len, const int *k, const double *v, const int y) {
+    // grad
+    double wx = dot_dss(ptrModel->w, k, v, len);
+    scale_dot_dss(ptrModel->temp_v, k, 0.9, len);
+    double sig = sigma(-wx * y);
+    double *temp;
+    memcpy(temp, v, ptrModel->nDims * sizeof(float8)); 
+    scale_dot_dss(temp, k, y*sig, len);
+    scale_dot_dss(temp, k, 0.1, len);
+    add_vector_dss(ptrModel->temp_v, k, temp, len);
+    add_and_scale_dss(ptrModel->w, k, ptrModel->temp_v, len, -1*ptrModel->stepsize);
+    //double c = ptrModel->stepsize * y * sig; // scale factor
+    //add_and_scale_dss(ptrModel->w, k, v, len, c);
+    
+    // regularization
+    double u = ptrModel->mu * ptrModel->stepsize;
+    l1_shrink_mask(ptrModel->w, u, k, len);
+}
+
+
+inline void
+dense_logit_grad(struct LinearModel *ptrModel, const double *v, const int y) {
+    // read and prepare
+    double wx = dot(ptrModel->w, v, ptrModel->nDims);
+    scale_dot(ptrModel->temp_v, 0.9, ptrModel->nDims); // beta * v_dw
+    double sig = sigma(-wx * y);
+    //double c = ptrModel->stepsize * y * sig; // scale factor
+    //add_and_scale(ptrModel->w, ptrModel->nDims, v, c);
+    double temp[ptrModel->nDims];
+    memcpy(&temp, v, ptrModel->nDims * sizeof(float8));   // v is feature value, not v_dw
+    scale_dot(temp, y*sig, ptrModel->nDims); 
+    scale_dot(temp, -0.1, ptrModel->nDims);
+    add_vectors(ptrModel->temp_v, temp, ptrModel->nDims);
+    add_and_scale(ptrModel->w, ptrModel->nDims, ptrModel->temp_v, -1*ptrModel->stepsize);
+    // regularization
+    double u = ptrModel->mu * ptrModel->stepsize;
+    l1_shrink_mask_d(ptrModel->w, u, ptrModel->nDims);
+}
+
+/**
 inline void
 sparse_logit_grad(struct LinearModel *ptrModel, const int len, const int *k, const double *v, const int y) {
     // grad
@@ -55,7 +97,7 @@ dense_logit_grad(struct LinearModel *ptrModel, const double *v, const int y) {
     double u = ptrModel->mu * ptrModel->stepsize;
     l1_shrink_mask_d(ptrModel->w, u, ptrModel->nDims);
 }
-
+**/
 inline double
 sparse_logit_loss(struct LinearModel *ptrModel, const int len, const int *k, const double *v, const int y) {
     double wx = dot_dss(ptrModel->w, k, v, len);
